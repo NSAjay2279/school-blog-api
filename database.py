ffa478a24b113c2fa6f44c0be757ed7f2e9b8a9f
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-12-05 17:49:46 +0000

    Thu, Dec 5, 2024, 11:19 PM +05:30

--- database.py
+++
@@ -1,5 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-
-MONGO_DETAILS = "mongodb://localhost:27017"  # Replace with your MongoDB URI
-client = AsyncIOMotorClient(MONGO_DETAILS)
-db = client["school_blog"]
