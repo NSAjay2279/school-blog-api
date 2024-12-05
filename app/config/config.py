37f0523f3f79c283aaf421cd2a26ada2af662389
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-12-05 18:38:57 +0000

    Fri, Dec 6, 2024, 12:08 AM +05:30

--- app/config/config.py
+++ app/config/config.py
@@ -8,6 +8,10 @@ uri = "mongodb+srv://negisinghajayus:Firstapp@pymongo.3xdtt.mongodb.net/?retryWr
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 
+db = client.schoolBlogs
+
+blogsCollection = db['Blogs']
+usersCollection = db['users']
 # Send a ping to confirm a successful connection
 try:
   client.admin.command('ping')
